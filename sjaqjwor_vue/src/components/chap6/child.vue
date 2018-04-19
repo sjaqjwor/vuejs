c147a4a3b95b910d300a11fe1971783fc300564b
sjaqjwor <lsklsk45@naver.com>
2018-04-19 15:17:41 +0900

    Create
    chap6 folder and finish props and event example1

---
+++ sjaqjwor_vue/src/components/chap6/child.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+    <button class="buttonstyle" v-on:click="clickevent" v-bind:data-lang="buttonInfo.value">
+        {{buttonInfo.text}}
+    </button>
+</div>
+ 
+</template>
+<script>
+export default {
+  name:"child",
+  props:['buttonInfo'],
+  methods:{
+      clickevent:function(e){
+          this.$emit('timeclick',e.target.innerText,e.target.dataset.lang);
+      }
+  }
+}
+</script>
+<style>
+.buttonstyle{
+    width:120px;height: 30px;text-align: center;
+}
+</style>
+
---
+++ sjaqjwor_vue/src/components/chap6/parent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <child v-for="s in buttons" :buttonInfo="s" @timeclick="timeclickEvent">
+      </child>
+      <hr />
+      <div>{{msg}}</div>
+  </div>
+</template>
+
+<script>
+import child from './child';
+
+export default {
+    name:"parent",
+    components:{
+        child
+    },
+    props:['buttons'],
+    data(){
+        return{
+            msg:""
+        }
+    },
+    methods:{
+        timeclickEvent:function(k,e){
+            this.msg=k+","+e;
+        }
+    }
+}
+</script>
