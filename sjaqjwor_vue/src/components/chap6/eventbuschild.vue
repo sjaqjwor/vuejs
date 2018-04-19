139af6b61315b92bee4ee8e6b04f9944333eeab6
sjaqjwor <lsklsk45@naver.com>
2018-04-19 16:30:02 +0900

    Finish
    eventBus and chap 6 finish

---
+++ sjaqjwor_vue/src/components/chap6/eventbuschild.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+  <button @click="event">child</button>
+  <div>{{currentTime}}</div>
+  </div>
+</template>
+
+<script>
+import {eventBus} from '../../main.js'
+export default {
+    name:"eventbuschild",
+    
+    data(){
+        return{
+            currentTime:''
+        }
+    },
+    methods:{
+        event:function(){
+            let d = new Date();
+            let t = d.toLocaleTimeString()+" "+d.getMilliseconds()+"ms";
+            eventBus.$emit('click1',t);
+            this.currentTime=t;
+        }
+    }
+};
+</script>
+
+<style>
+
+</style>
---
+++ sjaqjwor_vue/src/components/chap6/eventbuschild2.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+      <ul>
+          <li v-for="t in timelist">{{t}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+import {eventBus} from '../../main.js'
+export default {
+    name:"eventbuschild2",
+    data(){
+        return{
+            timelist:[]
+        }
+    },
+    mounted:
+        function(params) {
+            eventBus.$on('click1',this.child1Click);    
+        }
+    ,
+    methods:{
+        child1Click:function(time){
+            this.timelist.push(time);
+        }
+    }
+}
+</script>
+
