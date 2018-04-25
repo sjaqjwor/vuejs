5664ac39ed34637caa861169315d842cfe6e679b
sjaqjwor <lsklsk45@naver.com>
2018-04-25 14:48:23 +0900

    Finish chap8

---
+++ webpacktest/src/index.js
@@ -0,0 +1,8 @@
+import calc from '.utils/utility';
+
+let x=5;
+let y=10;
+
+let str = '<h2>${x}+${y}=${calc.add(x,y)}</h2>';
+
+document.getElementById("app").innerHTML=str;
---
+++ webpacktest/webpack.config.js
@@ -0,0 +1,24 @@
+varwebpack = require('webpack');
+
+module.exports={
+    entry:{
+        main : __dirname+'/src/index.js'
+    },
+    output:{
+        path:__dirname+'/public/dist/',
+        filename:'[name].js',
+        publicPath : '/dist'
+    },
+    module:{
+        rules:[
+            {
+                test:/|.js$/,
+                loader : 'babel-loader',
+                exclude : /node_modules/
+            }
+        ]
+    },
+    plugins:[
+        new webpack.optimize.UglifyJsPlugin()
+    ]
+}
