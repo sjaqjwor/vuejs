48683b625e8a95c6be2e80dfa0e9d7e394d16576
sjaqjwor <lsklsk45@naver.com>
2018-05-02 15:53:58 +0900

    chap 9.2 finish

---
+++ chap9/toodlistapp/src/components/module1.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <button :class="$style.hand">버튼 적용</button>
+  </div>
+</template>
+
+<script>
+export default {
+    created(){
+        console.log(this.$style);
+    }
+}
+</script>
+
+<style module>
+    .hand{
+        cursor: pointer; background-color: purple; color: yellow
+    }
+</style>
